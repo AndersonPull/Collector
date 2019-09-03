75f6c6ba1cd45a1d4e089acfbaad28d1a2e3f3a9
AndersonSmaug <andersonoliveera@outlook.com>
2019-09-03 18:51:10 -0300

    Design tela de informacoes

--- Collector/ViewModels/Home/MainInfoViewModel.cs
+++ Collector/ViewModels/Home/MainInfoViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Windows.Input;
+using Collector.Models.Home;
+using Collector.Services.Home;
 using Collector.Services.Navigation;
 using Collector.ViewModels.Login;
+using DLToolkit.Forms.Controls;
 using Xamarin.Forms;
 
 namespace Collector.ViewModels.Home
@@ -9,9 +12,26 @@ namespace Collector.ViewModels.Home
     public class MainInfoViewModel : BaseVM
     {
         INavigationService _serviceNavigation;
+        MenuService _service;
+        public FlowObservableCollection<BotoesMateriaisModel> ItensMenu { get; set; }
+
         public MainInfoViewModel(INavigationService serviceNavigation)
         {
+
+            ItensMenu = new FlowObservableCollection<BotoesMateriaisModel>();
+            _service = new MenuService();
             _serviceNavigation = serviceNavigation;
+
+            CarregarMenu();
+        }
+
+        public void CarregarMenu()
+        {
+            var collection = _service.ItensMenu();
+            foreach (var item in collection)
+            {
+                ItensMenu.Add(item);
+            }
         }
 
         public ICommand SairCommand
