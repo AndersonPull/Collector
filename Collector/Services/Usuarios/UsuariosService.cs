38fe435fd51333a6bbc82c44fe03c51b89be04dc
AndersonCrook <andersonoliveera@outlook.com>
2019-11-05 13:35:50 -0300

    ...

--- Collector/Services/Usuarios/UsuariosService.cs
+++ Collector/Services/Usuarios/UsuariosService.cs
@@ -12,7 +12,7 @@ namespace Collector.Services.Usuarios
             {
                 new UsuarioModel { Id = 0 , Nome = "Marcia Silva Santos", Apelido = "Marcia Catadora", Resumo="", Historia="Sou catadora e amo o que faço, e não tem preço ajuda nosso mundo a melhorar", Imagem="fotoPerfil.png", Telefone="(11) 947743829", Cidade = "Osasco" },
                 new UsuarioModel { Id = 1 , Nome = "flavia Martins", Apelido = "Martins", Resumo="", Historia="Sou catadora e amo o que faço, e não tem preço ajuda nosso mundo a melhorar", Imagem="fotoPerfil.png", Telefone="(11) 947743333", Cidade = "Osasco" },
-                new UsuarioModel { Id = 2 , Nome = "Isis Oliveira", Apelido = "Isis", Resumo="", Historia="Sou catadora e amo o que faço, e não tem preço ajuda nosso mundo a melhorar", Imagem="fotoPerfil.png", Telefone="(11) 947743825", Cidade = "São Paulo" }
+               // new UsuarioModel { Id = 2 , Nome = "Isis Oliveira", Apelido = "Isis", Resumo="", Historia="Sou catadora e amo o que faço, e não tem preço ajuda nosso mundo a melhorar", Imagem="fotoPerfil.png", Telefone="(11) 947743825", Cidade = "São Paulo" }
             };
         }
     }
