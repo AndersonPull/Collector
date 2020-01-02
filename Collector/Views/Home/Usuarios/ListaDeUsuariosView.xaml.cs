41986eced891d4669d2c864505352616bf9b18dc
AndersonCrook <andersonoliveera@outlook.com>
2020-01-02 18:41:35 -0300

    ...

--- Collector/Views/Home/Usuarios/ListaDeUsuariosView.xaml.cs
+++ Collector/Views/Home/Usuarios/ListaDeUsuariosView.xaml.cs
@@ -12,8 +12,6 @@ namespace Collector.Views.Home.Usuarios
             NavigationPage.SetBackButtonTitle(this, "");
             InitializeComponent();
             BindingContext = new ListaDeUsuariosViewModel();
-
-
         }
     }
 }
