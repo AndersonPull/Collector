e39ab087a500f5c4af6cf81fb093324ccd5a9bcb
SM4UG <andersonoliveera@outlook.com>
2020-02-25 20:59:07 -0300

    Traducao da tela de login

--- Collector/_Helpers/_Translations/AppResources.pt.Designer.cs
+++ Collector/_Helpers/_Translations/AppResources.pt.Designer.cs
@@ -16,21 +16,21 @@ namespace Collector._Helpers._Translations {
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class AppResources_pt_BR {
+    internal class AppResources_pt {
         
         private static System.Resources.ResourceManager resourceMan;
         
         private static System.Globalization.CultureInfo resourceCulture;
         
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal AppResources_pt_BR() {
+        internal AppResources_pt() {
         }
         
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.Equals(null, resourceMan)) {
-                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("Collector._Helpers._Translations.AppResources.pt-br", typeof(AppResources_pt_BR).Assembly);
+                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("Collector._Helpers._Translations.AppResources.pt", typeof(AppResources_pt).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
