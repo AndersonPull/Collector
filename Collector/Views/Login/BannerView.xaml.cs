2ec6f3681554eb89948fe24d15528caed809c213
SM4UG <andersonoliveera@outlook.com>
2020-04-14 10:46:43 -0300

    animações do banner

--- Collector/Views/Login/BannerView.xaml.cs
+++ Collector/Views/Login/BannerView.xaml.cs
@@ -11,13 +11,17 @@ namespace Collector.Views.Login
             InitializeComponent();
 
             BarraProgresso.ProgressTo(1, 5000, Easing.Linear);
-            
+
+            Imagens.FadeTo(0, 0, Easing.SinInOut);
+
+            icons1.FadeTo(0, 0, Easing.SinInOut);
+            icons2.FadeTo(0, 0, Easing.SinInOut);
 
             Timer timer = new Timer();
 
             timer.Interval = 5000;
 
-            timer.Elapsed += OnTimedEvent;
+            timer.Elapsed += OnTimedEvent1;
 
             timer.AutoReset = true;
 
@@ -27,7 +31,7 @@ namespace Collector.Views.Login
 
             timer1.Interval = 10000;
 
-            timer1.Elapsed += OnTimedEvent1;
+            timer1.Elapsed += OnTimedEvent2;
 
             timer1.AutoReset = true;
 
@@ -36,21 +40,6 @@ namespace Collector.Views.Login
 
         private void OnTimedEvent1(object sender, ElapsedEventArgs e)
         {
-            BarraProgresso3.ProgressTo(1, 5000, Easing.Linear);
-
-            Text2.TranslateTo(0, 0, 500, Easing.Linear);
-            Text2.FadeTo(0, 400, Easing.SinInOut);
-
-            Text4.TranslateTo(0, 250, 500, Easing.Linear);
-
-            Planet.TranslateTo(0, 350, 500, Easing.Linear);
-            Planet.FadeTo(0, 600, Easing.SinInOut);
-
-
-        }
-
-        private void OnTimedEvent(object sender, ElapsedEventArgs e)
-        {
             BarraProgresso2.ProgressTo(1, 5000, Easing.Linear);
 
             Text1.TranslateTo(-500, 0, 500, Easing.Linear);
@@ -58,13 +47,41 @@ namespace Collector.Views.Login
             Text2.TranslateTo(320, 0, 500, Easing.Linear);
 
             Text3.TranslateTo(70, 0, 500, Easing.Linear);
-
             Text3.FadeTo(0, 600, Easing.SinInOut);
 
-            Planet.TranslateTo(-80, 150, 500, Easing.Linear);
 
+            Imagens.FadeTo(1, 500, Easing.SinInOut);
 
+            Planet.TranslateTo(-80, 175, 500, Easing.Linear);
             Planet.ScaleTo(2.5, 500);
+        }
+
+        private void OnTimedEvent2(object sender, ElapsedEventArgs e)
+        {
+            BarraProgresso3.ProgressTo(1, 5000, Easing.Linear);
+
+            Text2.TranslateTo(0, 0, 500, Easing.Linear);
+            Text2.FadeTo(0, 400, Easing.SinInOut);
+
+            Text4.TranslateTo(0, 250, 500, Easing.Linear);
+
+            Planet.TranslateTo(0, 350, 400, Easing.Linear);
+            Planet.FadeTo(0, 600, Easing.SinInOut);
+
+            Imagens.TranslateTo(0, 560, 5000, Easing.Linear);
+            Imagens.FadeTo(0, 300, Easing.SinInOut);
+
+            Setas.TranslateTo(130, 0, 500,Easing.Linear);
+
+            icons1.FadeTo(1, 700, Easing.SinInOut);
+            icons1.ScaleTo(2, 1000);
+            icons1.ScaleTo(1, 1000, Easing.SpringOut);
+
+            icons2.FadeTo(1, 700, Easing.SinInOut);
+            icons2.ScaleTo(2, 1000);
+            icons2.ScaleTo(1, 1000, Easing.SpringOut);
+
+
         }
     }
 }
